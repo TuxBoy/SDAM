8bf55e2a5a5809d86e51e559abaeb47dc23d38fa
TuxBoy <benoit.mallo@gmail.com>
2018-07-29 22:23:35 +0200

    feat(add timestamp trait)

---
+++ example/Entity/Question.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Entity;
+
+use SDAM\Traits\HasTimestamp;
+
+/**
+ * Class Question
+ *
+ * @storeName questions_table
+ */
+class Question
+{
+    use HasTimestamp;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $slug;
+
+    /**
+     * @store false
+     * @var string
+     */
+    public $tmp_property;
+
+    /**
+     * @link belongsTo
+     * @var Response
+     */
+    public $response;
+
+    /**
+     * @text
+     * @var string
+     */
+    public $content;
+}
---
+++ example/Entity/Response.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Entity;
+
+use DateTime;
+
+class Response
+{
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var DateTime
+     */
+    public $createdAt;
+
+}
